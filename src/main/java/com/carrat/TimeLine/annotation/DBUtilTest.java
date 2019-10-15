8b03cd7833faec1f4583cf0ff7d97ad378e016ea
carrot <DELL@DESKTOP-9CM0O04>
2019-10-15 14:30:39 +0800

    丰富了注解的文档

--- src/main/java/com/carrat/TimeLine/annotation/DBUtilTest.java
+++ src/main/java/com/carrat/TimeLine/annotation/DBUtilTest.java
@@ -2,7 +2,8 @@ package com.carrat.TimeLine.annotation;
 @JDBCConfig(ip = "127.0.0.1", database = "test", encoding = "UTF-8", loginName = "root", password = "admin")//注解
 public class DBUtilTest {
 	public static void main(String[] args) {
-		JDBCConfig config = DBUtilTest.class.getAnnotation(JDBCConfig.class);//获取当前注解
+		//接下来就通过反射，获取这个DBUtil这个类上的注解对象
+		JDBCConfig config = DBUtilTest.class.getAnnotation(JDBCConfig.class);//获取当前注解 
         String ip = config.ip();//
         int port = config.port();
         String database = config.database();
--- src/main/java/com/carrat/TimeLine/annotation/JDBCConfig.java
+++ src/main/java/com/carrat/TimeLine/annotation/JDBCConfig.java
@@ -11,10 +11,10 @@ import java.lang.annotation.Target;
  * @author DELL 自定义jdbc mysql链接属性
  *
  */
-@Target({ElementType.METHOD,ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-@Inherited
-@Documented
+@Target({ElementType.METHOD,ElementType.TYPE})// 表示这个注解可以用用在类/接口上，还可以用在方法上
+@Retention(RetentionPolicy.RUNTIME)// 表示这是一个运行时注解，即运行起来之后，才获取注解中的相关信息，而不像基本注解如@Override 那种不用运行，在编译时eclipse就可以进行相关工作的编译时注解。
+@Inherited//表示这个注解可以被子类继承
+@Documented//表示当执行javadoc的时候，本注解会生成相关文档
 public @interface JDBCConfig {
      String ip();
      int port() default 3306;
