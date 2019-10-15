18adef03e22f95e2810bebc62f4667b9157e3b8f
carrot <DELL@DESKTOP-9CM0O04>
2019-10-15 11:24:51 +0800

    注解的基本使用，获取注解的属性和属性值

---
+++ src/main/java/com/carrat/TimeLine/annotation/DBUtilTest.java
@@ -0,0 +1,20 @@
+package com.carrat.TimeLine.annotation;
+@JDBCConfig(ip = "127.0.0.1", database = "test", encoding = "UTF-8", loginName = "root", password = "admin")//注解
+public class DBUtilTest {
+	public static void main(String[] args) {
+		JDBCConfig config = DBUtilTest.class.getAnnotation(JDBCConfig.class);//获取当前注解
+        String ip = config.ip();//
+        int port = config.port();
+        String database = config.database();
+        String encoding = config.encoding();
+        String loginName = config.loginName();
+        String password = config.password();
+        //输出
+        System.out.println(ip);
+        System.out.println(port);
+        System.out.println(database);
+        System.out.println(encoding);
+        System.out.println(loginName);
+        System.out.println(password);
+	}
+}
---
+++ src/main/java/com/carrat/TimeLine/annotation/JDBCConfig.java
@@ -0,0 +1,25 @@
+package com.carrat.TimeLine.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author DELL 自定义jdbc mysql链接属性
+ *
+ */
+@Target({ElementType.METHOD,ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+public @interface JDBCConfig {
+     String ip();
+     int port() default 3306;
+     String database();
+     String encoding();
+     String loginName();
+     String password();
+}
